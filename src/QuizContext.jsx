f86bac77631f13a18ae33790003489f19aacd542
vphuc02 <levanphuc11052002@gmail.com>
2026-06-19 08:49:04 +0700

    update lab4

--- src/QuizContext.jsx
+++ src/QuizContext.jsx
@@ -3,7 +3,6 @@ import React, { createContext, useState } from 'react';
 export const QuizContext = createContext();
 
 export const QuizProvider = ({ children }) => {
-  // Quản lý câu trả lời mà người dùng đang chọn (input)
   const [selectedAnswer, setSelectedAnswer] = useState('');
 
   return (
